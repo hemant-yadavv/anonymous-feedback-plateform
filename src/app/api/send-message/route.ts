fab69e9788d8bc7a78c0f5c88433c98acaf734bc
Hemant <hemantyadav1137@gmail.com>
2024-07-30 19:21:55 +0530

    accept-messages, get-messages and send-messsages routes

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,51 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(request: Request) {
+    await dbConnect()
+
+    const { username, content } = await request.json()
+
+    try {
+        const user = await UserModel.findOne({ username })
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: 'User Not Found'
+                }, { status: 404 }
+            );
+        }
+
+        if (!user.isAcceptingMessage) {
+            return Response.json(
+                {
+                    success: false,
+                    message: 'User is not accepting the messages'
+                }, { status: 403 }
+            );
+        }
+
+        const newMessage = { content, createdAt: new Date() }
+
+        user.messages.push(newMessage as Message)
+        await user.save()
+
+        return Response.json(
+            {
+                success: true,
+                message: 'Message sent successfully'
+            }, { status: 200 }
+        );
+
+    } catch (error) {
+        console.error('Error senting messages:', error);
+        return Response.json(
+            {
+                message: 'Internal server error',
+                success: false
+            }, { status: 500 }
+        );
+    }
+}
